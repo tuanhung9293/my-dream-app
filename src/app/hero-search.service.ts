660cbf48886cf81325e8679145d21ffaf30ad7ba
Viet Tuan <tuanhung9293@gmail.com>
2017-06-29 08:03:10 +0700

    search

--- src/app/hero-search.service.ts
+++ src/app/hero-search.service.ts
@@ -10,10 +10,13 @@ import { Hero }           from './hero';
 export class HeroSearchService {
 
   constructor(private http: Http) {}
+  private heroesUrl = 'https://angular-hero.herokuapp.com/heros/?name';
 
   search(term: string): Observable<Hero[]> {
+  	const url = `${this.heroesUrl}=${term}`;
     return this.http
-      .get(`api/heroes/?name=${term}`)
-      .map(response => response.json().data as Hero[]);
+	  // .get(`api/heroes/?name=${term}`)
+	  .get(`url`)
+	  .map(response => response.json().data as Hero[]);
   }
 }
