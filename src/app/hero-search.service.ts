550643314659e1afd88a2adcd1d5df554f5fa924
Viet Tuan <tuanhung9293@gmail.com>
2017-06-29 10:15:04 +0700

    search api

--- src/app/hero-search.service.ts
+++ src/app/hero-search.service.ts
@@ -8,13 +8,10 @@ import { Hero } from './hero';
 
 @Injectable()
 export class HeroSearchService {
-  private heroesUrl = 'https://angular-hero.herokuapp.com/heros/?name';
-
   constructor(private http: Http) {}
   search(term: string): Observable<Hero[]> {
-    const url = `${this.heroesUrl}=${term}`;
     return this.http
-    .get(url)
+    .get(`http://angular-hero.herokuapp.com/search?name=${term}`)
     .map(response => response.json() as Hero[]);
   }
 }
